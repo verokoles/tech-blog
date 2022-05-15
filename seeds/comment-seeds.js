bc2d46ef7036c8e85f3293cf72f54dc004a5bcf3
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-05-15 17:49:08 -0500

    push heroku

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -7,8 +7,8 @@ const commentData = [
     comment_text: "Love seeing updates on this!"
   },
   {
-    user_id: 1,
-    post_id: 1,
+    user_id: 2,
+    post_id: 2,
     comment_text:
       "Thank you for posting!"
   },
@@ -23,13 +23,13 @@ const commentData = [
     comment_text: "Wow, this is outstanding!"
 },
 {
-    user_id: 2,
-    post_id: 2,
+    user_id: 5,
+    post_id: 5,
     comment_text: "It was an honor being part of this hardoworking team!"
 },
 {
-    user_id: 5,
-    post_id: 5,
+    user_id: 6,
+    post_id: 6,
     comment_text: "Impressive! Great job!!"
 }
 ];
